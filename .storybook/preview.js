7568c2f12db319d49ef648b2505e494512bb410b
zhouqijun <jackchou4job@163.com>
2022-10-19 17:43:09 +0800

    feat: 引入element-plus

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,18 @@
+/*
+ * @Date        : 2022-10-19 15:13:08
+ * @Author      : ZhouQijun
+ * @LastEditors : ZhouQijun
+ * @LastEditTime: 2022-10-19 16:36:17
+ * @Description :
+ */
+import { app } from '@storybook/vue3'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+app.use(ElementPlus)
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
