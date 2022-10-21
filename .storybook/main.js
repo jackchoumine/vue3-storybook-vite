6d844a1d47ad5b8a8b10b92083620abedaba5aba
zhouqijun <jackchou4job@163.com>
2022-10-21 11:46:15 +0800

    解决打包错误

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,24 @@
+/*
+ * @Date        : 2022-10-19 15:13:08
+ * @Author      : ZhouQijun
+ * @LastEditors : ZhouQijun
+ * @LastEditTime: 2022-10-21 11:36:11
+ * @Description :
+ */
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/vue3",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: '@storybook/vue3',
+  core: {
+    //NOTE 打包时遇到错误
+    // "builder": "@storybook/builder-vite"
+    builder: 'webpack5',
   },
-  "features": {
-    "storyStoreV7": true
-  }
+  features: {
+    storyStoreV7: true,
+  },
 }
--- README.md
+++ README.md
@@ -14,6 +14,28 @@ yarn
 yarn storybook
 ```
 
+## 遇到的问题
+
+> build-storybook error: The 'compilation' argument must be an instance of Compilation
+
+升级打包工具：
+
+```sh
+npm install @storybook/builder-webpack5 @storybook/manager-webpack5 --save-dev
+```
+
+修改 `./storybook/main.js`:
+
+```js
+ core: {
+    //NOTE 打包时遇到错误
+    // "builder": "@storybook/builder-vite"
+    builder: 'webpack5',
+  },
+```
+
+[问题详情](https://github.com/storybookjs/storybook/issues/17014#issuecomment-1256904918)
+
 ## 参考
 
 [Getting started with Storybook in Vue 3](https://blog.logrocket.com/getting-started-storybook-vue-3/)
